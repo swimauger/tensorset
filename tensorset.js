1b92be772ef0aed0b5c4b6a13c4e192ebc4a65a1
swimauger <swimauger@gmail.com>
2020-07-17 17:38:07 -0500

    Release Tensorset v1.0.0

---
+++ tensorset.js
@@ -0,0 +1,26 @@
+const tf = require('@tensorflow/tfjs');
+
+class Tensorset {
+    static parse(data) {
+        data = JSON.parse(data);
+        const dataset = {};
+        for (const example of data) {
+            dataset[example.label] = tf.tensor(example.values, example.label);
+        }
+        return dataset;
+    }
+
+    static async stringify(dataset) {
+        const data = [];
+        for (const label in dataset) {
+            data.push({
+                label,
+                values: Array.from(await dataset[label].data()),
+                shape: dataset[label].shape
+            });
+        }
+        return JSON.stringify(data);
+    }
+}
+
+module.exports = Tensorset;
