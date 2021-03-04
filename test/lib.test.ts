453a90b1363cdb8a44b23befc8ebb955024d410a
swimauger <swimauger@gmail.com>
2021-03-04 12:16:23 -0600

    Add type definitions 📖

--- test/lib.test.ts
+++ test/lib.test.ts
@@ -2,24 +2,24 @@ import * as fs from "fs/promises";
 import * as path from "path";
 import Tensorset from "../Tensorset";
 
-const DATASET_PATH = path.resolve(__dirname, 'dataset.json');
+const DATASET_PATH: string = path.resolve(__dirname, 'dataset.json');
 
 describe('tensorset library tests', () => {
   test('tensorset parse legitimate dataset', async () => {
-    const data = await fs.readFile(DATASET_PATH, { encoding: 'utf-8' });
+    const data: string = await fs.readFile(DATASET_PATH, { encoding: 'utf-8' });
     expect(Tensorset.parse(data)).toBeDefined();
   });
 
   test('tensorset parse illegitimate dataset', () => {
-      try {
-        Tensorset.parse('{"a": { "b": "c" }}');
-      } catch (error) {
-        expect(error).toBeInstanceOf(TypeError);
-      }
+    try {
+      Tensorset.parse('{"a": { "b": "c" }}');
+    } catch (error) {
+      expect(error).toBeInstanceOf(TypeError);
+    }
   });
 
   test('tensorset stringify legitimate dataset', async () => {
-    const data = await fs.readFile(DATASET_PATH, { encoding: 'utf-8' });
+    const data: string = await fs.readFile(DATASET_PATH, { encoding: 'utf-8' });
     const dataset = Tensorset.parse(data);
     expect(Tensorset.stringify(dataset)).resolves.toBeDefined();
   });
