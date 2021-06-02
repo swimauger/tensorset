29db485a6650f768acace718ed32b60b32202f75
swimauger <swimauger@gmail.com>
2021-06-02 13:29:14 -0500

    🐛 Adjust tensor types for parse

--- Tensorset.ts
+++ Tensorset.ts
@@ -1,11 +1,11 @@
 import * as tf from '@tensorflow/tfjs';
-import {Tensor2D} from '@tensorflow/tfjs';
+import * as tfCore from '@tensorflow/tfjs-core';
 
 /**
 * Tensorset is a class used similarly to JSON for stringifying
 * and parsing Google's KNN Classifier datasets
 * @author Mark Auger
-* @version 1.2.6
+* @version 1.2.7
 * @social Follow me on GitHub at https://github.com/swimauger
 * @license
 * Copyright (c) 2020 Mark Auger
@@ -38,9 +38,9 @@ class Tensorset {
   * @param {string} data - Stringified tensorset file generated with assistance of `Tensorset.stringify`
   * @example
   * const dataset = Tensorset.parse(data);
-  * @returns { [label: string]: Tensor2D } Returns KNN Classifier readable dataset
+  * @returns { [label: string]: tf.Tensor | tfCore.Tensor } Returns KNN Classifier readable dataset
   **/
-  static parse(data: string): { [label: string]: Tensor2D } {
+  static parse(data: string): { [label: string]: tf.Tensor | tfCore.Tensor } {
     return JSON.parse(data).reduce(
         (dataset: any, {label, values, shape}: any) => {
           return {
